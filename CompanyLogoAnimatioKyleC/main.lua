d9daf83a923fa18085f07fa4904bacfc07792ab8
Kylechristensen8842 <43419336+Kylechristensen8842@users.noreply.github.com>
2018-11-09 14:29:15 -0500

    added company logo animation

--- CompanyLogoAnimatioKyleC/main.lua
+++ CompanyLogoAnimatioKyleC/main.lua
@@ -1,7 +1,10 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: CompanyLogoAnimation
+-- Name: Your Name
+-- Course: ICS2O/3C
+-- This program makes my company logo fly across the screen and there is a sound when it happens
 
--- Your code here
+--hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+--sets the background colour
+display.setDefault("background", 100/255, 0/255, 100/255)
